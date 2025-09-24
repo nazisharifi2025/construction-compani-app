a353b9d5faa1a15eb0b2309dce22d81d753b288a
nazi sharifi <banonazisharifi@gmail.com>
2025-09-24 23:46:16 +0430

    hero added

--- src/MyComponents/Hero.tsx
+++ src/MyComponents/Hero.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import type { Settings } from "react-slick";
 function Hero() {
-  const settings = {
+  const settings: Settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 2,
+    initialSlide: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1500,
  responsive: [
   {
     breakpoint: 1024,
@@ -39,64 +43,79 @@ function Hero() {
   },
 ],
   };
-
+  const Cards = [
+    {
+      name: "Ali Rezaiy",
+      Job : "engenearing",
+      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      imgUrl : "/images/hero3.jpg"
+    },
+    {
+      name: "Danial Ebrahimi",
+      Job : "engenearing",
+      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      imgUrl : "/images/hero4.jpg"
+    },
+    {
+      name: "Farhad Sadiqi",
+      Job : "engenearing",
+      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      imgUrl : "/images/hero-2.jpg"
+    },
+    {
+      name: "Sabir Ahmadi",
+      Job : "engenearing",
+      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      imgUrl : "/images/hero5.jpg"
+    },
+    {
+      name: "Shadmihr vafa",
+      Job : "engenearing",
+      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      imgUrl : "/images/hero6.jpg"
+    },
+  ]
   return (
     <div className="w-full lg:h-screen h-[110vh] bg-no-repeat bg-cover bg-center bg-[url('/images/bg-hero.jpg')]">
       <div className="flex lg:pt-24 py-24 w-full h-full bg-stone-100/85 lg:flex-row flex-col gap-0">
         {/* Left Side */}
-        <div className="h-full flex-1 p-3 justify-center lg:items-start items-center flex flex-col">
-          <h1 className="lg:text-6xl md:text-5xl text-2xl md:text-left text-center  font-bold leading-tight">
+        <div className="h-full flex-1 p-3 mt-18 justify-center lg:items-start items-center flex flex-col">
+          <h1 className="lg:text-6xl moto md:text-5xl text-2xl md:text-left text-center  font-bold leading-tight">
             Build Your Dream <br /> House With Us
           </h1>
           <p className="text-gray-600 my-6 md:text-left text-center">
             We are committed to providing our clients with exceptional service
             while <br /> offering our employees the best training.
           </p>
-          <button className="px-6 py-3 bg-amber-600 text-white w-fit rounded-md hover:bg-amber-700 transition-all duration-300 ease-in-out">
-            Get A Quote
+          <div className="flex space-x-5">
+          <button className="px-6 py-2.5 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
+            Services
           </button>
+          <button className="px-6 py-2.5 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
+            Explore More
+          </button>
+          </div>
         </div>
-
+        
         {/* Right Side */}
-        <div className="flex-1 h-full flex flex-col justify-center items-center">
-          <h1 className="font-bold text-2xl mb-4">Our Engineers</h1>
+        <div className="flex-1 h-full flex flex-col mt-18 justify-center items-center">
+          <h1 className="font-bold text-3xl mb-4">Our Expert Team</h1>
           <div className="lg:w-[600px] md:w-[600px] w-[90%]">
             <Slider {...settings}>
-              <div className="p-2 lg:w-[300px] md:w-[300px] w-full h-[370px]">
+              {Cards.map((item , index)=>(
+                   <div key={index} className="p-2 group relative lg:w-[300px] md:w-[300px] w-full h-[400px]">
                 <img
-                  src="/images/hero3.jpg"
+                  src={item.imgUrl}
                   alt="hero3"
                   className="rounded-md shadow-md h-full w-full"
                 />
+                <div className="h-[385px] w-[285px] flex opacity-0 rounded-md bg-black/50 absolute  flex-col gap-3 text-white -bottom-[385px] group-hover:bottom-2 group-hover:opacity-100 transition-all duration-500 ease-initial right-2 justify-center items-center">
+                <h1 className="text-2xl">{item.name}</h1>
+                <h2 className="text-xl">{item.Job}</h2>
+                <p className="w-[90%] mx-auto text-center">{item.des}</p>
+                </div>
               </div>
-              <div className="p-2 lg:w-[300px] md:w-[300px] w-full h-[370px]">
-                <img
-                  src="/images/hero4.jpg"
-                  alt="hero4"
-                  className="rounded-md shadow-md h-full w-full"
-                />
-              </div>
-              <div className="p-2 lg:w-[300px] md:w-[300px] w-full h-[370px]">
-                <img
-                  src="/images/hero-2.jpg"
-                  alt="hero2"
-                  className="rounded-md shadow-md h-full w-full"
-                />
-              </div>
-              <div className="p-2 lg:w-[300px] md:w-[300px] w-full h-[370px]">
-                <img
-                  src="/images/hero5.jpg"
-                  alt="hero5"
-                  className="rounded-md shadow-md h-full w-full"
-                />
-              </div>
-              <div className="p-2 lg:w-[300px] md:w-[300px] w-full h-[370px]">
-                <img
-                  src="/images/hero6.jpg"
-                  alt="hero6"
-                  className="rounded-md shadow-md h-full w-full"
-                />
-              </div>
+              ))}
             </Slider>
           </div>
         </div>
