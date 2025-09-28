583360b281f3903b35ac1c7120ca262e25f5c2a4
nazi sharifi <banonazisharifi@gmail.com>
2025-09-28 22:28:39 +0430

    constaraction

--- src/MyComponents/Hero.tsx
+++ src/MyComponents/Hero.tsx
@@ -18,8 +18,8 @@ function Hero() {
   {
     breakpoint: 1024,
     settings: {
-      slidesToShow: 2,
-      slidesToScroll: 2,
+      slidesToShow: 1,
+      slidesToScroll: 1,
       infinite: true,
       dots: false,
     },
@@ -46,41 +46,41 @@ function Hero() {
   const Cards = [
     {
       name: "Ali Rezaiy",
-      Job : "engenearing",
-      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      Job : "Project Manager",
+      des : "With over 15 years of experience in managing large construction projects, he ensures quality control, proper scheduling, and successful project delivery.",
       imgUrl : "/images/hero3.jpg"
     },
     {
-      name: "Danial Ebrahimi",
-      Job : "engenearing",
-      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      name: "Narges Ahmadi",
+      Job : "Civil Engineer",
+      des : "A specialist in structural design and calculations, focusing on safety and international standards to guarantee the strength of all projects.",
       imgUrl : "/images/hero4.jpg"
     },
     {
-      name: "Farhad Sadiqi",
-      Job : "engenearing",
-      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      name: "َAli Karimi",
+      Job : "Site Supervisor",
+      des : "With extensive experience in managing on-site teams, he ensures smooth daily operations and strict adherence to safety principles.",
       imgUrl : "/images/hero-2.jpg"
     },
     {
       name: "Sabir Ahmadi",
-      Job : "engenearing",
-      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      Job : "Interior Designer",
+      des : "An expert in creating beautiful and functional spaces, with an emphasis on innovation and modern materials to meet client satisfaction.",
       imgUrl : "/images/hero5.jpg"
     },
     {
-      name: "Shadmihr vafa",
-      Job : "engenearing",
-      des : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo fugiat ullam sed nam tenetur exercitationem enim.",
+      name: "Mohammad Hosseini",
+      Job : "Architect",
+      des : "With a creative approach and unique designs, he brings both functionality and artistic elegance to every project.",
       imgUrl : "/images/hero6.jpg"
     },
   ]
   return (
-    <div className="w-full lg:h-screen h-[110vh] bg-no-repeat bg-cover bg-center bg-[url('/images/bg-hero.jpg')]">
-      <div className="flex lg:pt-24 py-24 w-full h-full bg-stone-100/85 lg:flex-row flex-col gap-0">
+    <div className="w-full lg:h-screen  bg-no-repeat bg-cover bg-center bg-[url('/images/bg-hero.jpg')]">
+      <div className="flex lg:pt-24 py-24 w-full h-full md:flex-row bg-stone-100/85 lg:flex-row md:flex-r flex-col gap-0">
         {/* Left Side */}
-        <div className="h-full flex-1 p-3 mt-18 justify-center lg:items-start items-center flex flex-col">
-          <h1 className="lg:text-6xl moto md:text-5xl text-2xl md:text-left text-center  font-bold leading-tight">
+        <div className="h-full md:gap-8 flex-1 min-w-0 p-3 mt-18 justify-center lg:items-start items-center flex flex-col">
+          <h1 className="lg:text-6xl moto md:text-4xl text-2xl md:text-left text-center  font-bold leading-tight">
             Build Your Dream <br /> House With Us
           </h1>
           <p className="text-gray-600 my-6 md:text-left text-center">
@@ -88,30 +88,30 @@ function Hero() {
             while <br /> offering our employees the best training.
           </p>
           <div className="flex space-x-5">
-          <button className="px-6 py-2.5 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
+          <button className="px-6 py-2.5 hover:outline-1 outline-amber-600 hover:outline-offset-4 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
             Services
           </button>
-          <button className="px-6 py-2.5 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
+          <button className="px-6 py-2.5 hover:outline-1 outline-amber-600 hover:outline-offset-4 bg-amber-600 text-white w-fit font-serif hover:bg-amber-700 transition-all duration-300 ease-in-out">
             Explore More
           </button>
           </div>
         </div>
         
         {/* Right Side */}
-        <div className="flex-1 h-full flex flex-col mt-18 justify-center items-center">
+        <div className="flex-1 min-w-0 h-full flex flex-col mt-18 justify-center items-center">
           <h1 className="font-bold text-3xl mb-4">Our Expert Team</h1>
-          <div className="lg:w-[600px] md:w-[600px] w-[90%]">
+          <div className="lg:w-[600px] md:w-[90%] w-[90%]">
             <Slider {...settings}>
               {Cards.map((item , index)=>(
                    <div key={index} className="p-2 group relative lg:w-[300px] md:w-[300px] w-full h-[400px]">
                 <img
                   src={item.imgUrl}
                   alt="hero3"
-                  className="rounded-md shadow-md h-full w-full"
+                  className="rounded-md shadow-md -z-50 h-full w-full"
                 />
-                <div className="h-[385px] w-[285px] flex opacity-0 rounded-md bg-black/50 absolute  flex-col gap-3 text-white -bottom-[385px] group-hover:bottom-2 group-hover:opacity-100 transition-all duration-500 ease-initial right-2 justify-center items-center">
+                <div className="h-[385px] w-[285px] flex opacity-0  rounded-md bg-black/50 absolute  flex-col gap-3 text-white -bottom-[385px] group-hover:bottom-2 group-hover:opacity-100 transition-all duration-500 ease-initial right-2 justify-center items-center">
                 <h1 className="text-2xl">{item.name}</h1>
-                <h2 className="text-xl">{item.Job}</h2>
+                <h2 className="text-xl text-amber-600">{item.Job}</h2>
                 <p className="w-[90%] mx-auto text-center">{item.des}</p>
                 </div>
               </div>
--- src/MyComponents/Navbar.tsx
+++ src/MyComponents/Navbar.tsx
@@ -4,9 +4,9 @@ import { useState } from 'react'
 function Navbar() {
     const [open,setOpen] = useState(false);
   return (
-    <div className='w-full fixed bg-stone-100 px-8 py-4 flex justify-between items-center'>
+    <div className='w-full fixed bg-stone-100 md:px-8 px-1 py-4 flex justify-between items-center'>
         <h1 className='text-2xl font-bold font-paci'>Sharifi Constrction Company</h1>
-        <ul id='ull' className={`${open ? 'flex' : 'hidden'} lg:flex-row flex-col lg:flex lg:bg-transparent lg:h-fit lg:w-fit lg:relative absolute lg:top-0 top-23 bg-stone-100 rounded-2xl rounded-t-none lg:p-0 p-6 w-[70%] lg:right-0 md:right-30 right-19 lg:items-center items-start lg:gap-0 gap-5 space-x-8`}>
+        <ul id='ull' className={`${open ? 'flex' : 'hidden'} lg:flex-row flex-col z-50 lg:flex lg:bg-transparent lg:h-fit lg:w-fit lg:relative absolute lg:top-0 md:top-18 top-15 bg-stone-100 rounded-2xl rounded-t-none lg:p-0 p-6 md:w-[70%] w-full lg:right-0 md:right-30  right-0 transition-all duration-700 lg:items-center items-start lg:gap-0 gap-5 space-x-8`}>
             <a className='hover:border-b font-bold border-gray-700 transition-all duration-1000 ease-in-out' href="/">Home</a>
             <a className='hover:border-b font-bold border-gray-700 transition-all duration-1000 ease-in-out' href="/About">About Us</a>
             <a className='hover:border-b font-bold border-gray-700 transition-all duration-1000 ease-in-out' href="/Serveces">Serveces</a>
@@ -14,8 +14,8 @@ function Navbar() {
             <a className='hover:border-b font-bold border-gray-700 transition-all duration-1000 ease-in-out' href="/Contuct">Contuct Us</a>
         </ul>
         <div className='flex space-x-8'>
-        <button className='px-6 py-2 rounded-md outline-1 hover:outline-0 hover:bg-gray-300/40'>Wether</button>
-        <button  >
+        <button className='px-6 py-2 rounded-md outline-1 hover:outline-0 hover:bg-gray-300/40 hidden md:block'>Wether</button>
+        <button  className='lg:hidden block'>
              <Menu onClick={()=> setOpen(!open)} className={`${!open ? "block" : "hidden"}`} size={24} />
              <X onClick={()=> setOpen(!open)} className={`${open ? "block" : "hidden"}`} size={24}/>
         </button>
